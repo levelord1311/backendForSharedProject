6f2067b02e2ab7bc79df0a7a5ff53777331f638e
levelord1311 <rmv1311@gmail.com>
2022-11-10 07:46:53 +0300

    time parse layout fix

--- internal/app/model/rawTime.go
+++ internal/app/model/rawTime.go
@@ -5,5 +5,5 @@ import "time"
 type RawTime []byte
 
 func (t *RawTime) Time() (time.Time, error) {
-	return time.Parse("15:04:05", string(*t))
+	return time.Parse("2006-01-02 15:04:05", string(*t))
 }
